81d4c5bda071ff15d6997b16f58972245645bf27
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-02 13:15:47 -0500

    Adding database???

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1>Home</h1>
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{"C:\Users\trott\Downloads\DigiCertGlobalRootCA.crt.pem"}", NULL, NULL);
+mysqli_real_connect($conn, "hw3database.mysql.database.azure.com", "tstew22", "{database_4013}", "{DBHW3}", 3306, MYSQLI_CLIENT_SSL);
 <?php
 include "view-footer.php";
 ?>
