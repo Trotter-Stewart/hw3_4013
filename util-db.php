032e48ff3d242a0d12362d112ed8b4d3255921a4
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-14 13:48:19 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('hw3database.mysql.database.azure.com', 'tstew22', 'database_4013', 'DBHW3');
+    $conn = new mysqli('hw3database.mysql.database.azure.com', 'tstew22', 'database_4013', 'dogs');
     
     // Check connection
     if ($conn->connect_error) {
