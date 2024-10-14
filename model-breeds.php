1da02b06e37750872367e523e232aae1c533c958
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-14 13:24:59 -0500

    Create model-breeds.php

---
+++ model-breeds.php
@@ -0,0 +1,19 @@
+<?php
+
+function selectBreeds() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select * from breed");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+
+?>
