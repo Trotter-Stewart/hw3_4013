400376c0c247f75caf35b8722f50b531b9784a50
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-20 13:33:06 -0500

    Create view-size.php

---
+++ view-size.php
@@ -0,0 +1,24 @@
+<h1>Colors</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>SizeID</th>
+         <th>SizeName</th>
+      </tr>
+     
+    </thead>
+    <tbody>
+      <?php
+    while ($size = $sizes->fetch_assoc()) {
+        ?>
+        <tr>
+      <td><?php echo $size['idsize']; ?></td>
+      <td><?php echo $size['sizename']; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
