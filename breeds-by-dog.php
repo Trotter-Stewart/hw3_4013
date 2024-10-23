d1c4b72f25315dba5620009f01bb8e48858d78c6
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-23 11:56:38 -0500

    Update breeds-by-dog.php

--- breeds-by-dog.php
+++ breeds-by-dog.php
@@ -4,7 +4,7 @@ require_once("model-breeds-by-dog.php");
 $pageTitle = "Breeds by Dog";
 include "view-header.php";
 
-$breeds = selectBreedsByDog($_GET['dogbreed']);
+$breeds = selectBreedsByDog($_GET['iddog']);
 include "view-breeds-by-dog.php";
 
 include "view-footer.php";
