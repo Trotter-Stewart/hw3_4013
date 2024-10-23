fba8c4bc775542f96d5819102b2e84848d65e91f
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-23 13:11:39 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -33,7 +33,7 @@
         </li>
 
             <li class="nav-item">
-          <a class="nav-link" href="dogs-with-size.php">Dogs with size</a>
+          <a class="nav-link" href="dogs-with-breed.php">Dogs with breed</a>
         </li>
       </ul>
     </div>
