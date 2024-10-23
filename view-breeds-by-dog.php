01f21fa10425289a35a57009060c00930dad6392
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-23 10:38:21 -0500

    Create view-breeds-by-dog.php

---
+++ view-breeds-by-dog.php
@@ -0,0 +1,24 @@
+<h1>Breeds</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>BreedID</th>
+         <th>BreedName</th>
+      </tr>
+     
+    </thead>
+    <tbody>
+      <?php
+      while ($breed = $breeds->fetch_assoc()) {
+        ?>
+        <tr>
+          <td><?php echo $breed['idbreed']; ?></td>
+          <td><?php echo $breed['breedname']; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
