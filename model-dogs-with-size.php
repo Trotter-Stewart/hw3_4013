561cd79ccb78e57e663edde33fd3c3a6e2d2c54b
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-23 12:36:25 -0500

    Create model-dogs-with-size.php

---
+++ model-dogs-with-size.php
@@ -0,0 +1,15 @@
+<?php
+
+function selectDogs() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select iddog, dogname, dogcol, dogbreed, dogsize from dog");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
