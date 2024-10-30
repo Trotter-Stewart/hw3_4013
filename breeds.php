e70b9eb66d17ad51a152bfd75a49967227042635
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-30 18:13:41 -0500

    Update breeds.php

--- breeds.php
+++ breeds.php
@@ -4,6 +4,14 @@ require_once("model-breeds.php");
 $pageTitle = "Breeds";
 include "view-header.php";
 
+if isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+  case "Add":
+    insertBreeds($_POST['breedName']);
+    break;
+  }
+}
+
 $breeds = selectBreeds();
 include "view-breeds.php";
 
