1a817077b8ed46b8790f19ba439c3bb5655407fd
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-30 14:44:02 -0500

    Update model-dogs-with-breeds.php

--- model-dogs-with-breeds.php
+++ model-dogs-with-breeds.php
@@ -12,4 +12,19 @@ function selectDogs() {
         throw $e;
     }
 }
+
+function selectBreedsByDog($id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select idbreed, breedname from breed b join dog d on b.idbreed = d.dogbreed where iddog = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
