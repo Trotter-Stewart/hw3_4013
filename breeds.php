cd5f862226f6362258938d2d142a0a421b477de9
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-10-30 18:39:29 -0500

    Update breeds.php

--- breeds.php
+++ breeds.php
@@ -7,8 +7,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
   case "Add":
-    insertBreeds($_POST['breedName']);
+    if (insertBreeds($_POST['breedName']))
+    {
+        echo '<div class="alert alert-success" role="alert">Breed added.</div>"';
+    }else{
+      echo '<div class="alert alert-danger" role="alert">Error.</div>"';
+    }
     break;
+    
   }
 }
 
