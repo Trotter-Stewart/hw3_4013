5baf26cc3020a24783aae62f52fb0d348b3e3a32
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-03 20:34:53 -0600

    Update model-size.php

--- model-size.php
+++ model-size.php
@@ -14,6 +14,49 @@ function selectSize() {
     }
 }
 
+function insertSizes($nameSize) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO size (sizename) VALUES (?)");
+        $stmt->bind_param("s", $nameSize);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSizes($nameSize, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update size set sizename = ? where (idsize = ?)");
+        $stmt->bind_param("si", $nameColor, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteColors($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from size where idsize = ?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 
 
 ?>
