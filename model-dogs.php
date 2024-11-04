9e18b977859a35e7ca89512df390114815b74945
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-03 20:38:23 -0600

    Update model-dogs.php

--- model-dogs.php
+++ model-dogs.php
@@ -14,6 +14,48 @@ function selectDogs() {
     }
 }
 
+function insertDogs($nameDog) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO dog (sizename) VALUES (?)");
+        $stmt->bind_param("s", $nameSize);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSizes($nameSize, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update size set sizename = ? where (idsize = ?)");
+        $stmt->bind_param("si", $nameSize, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteSizes($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from size where idsize = ?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 
 
 ?>
