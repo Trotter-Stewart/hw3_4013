978a368adb12c4e7be5764e106bed1038a5f592d
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-03 20:32:11 -0600

    Update model-colors.php

--- model-colors.php
+++ model-colors.php
@@ -14,6 +14,48 @@ function selectColors() {
     }
 }
 
+function insertColors($nameColor) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO color (colorname) VALUES (?)");
+        $stmt->bind_param("s", $nameColor);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateColors($nameColor, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update color set colorname = ? where (idcolor = ?)");
+        $stmt->bind_param("si", $nameColor, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteColors($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from color where idcolor = ?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 
 
 ?>
