90bb6cfc63ecdbe29473d688c4f18e836a63f172
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-04 17:33:37 -0600

    Update size.php

--- size.php
+++ size.php
@@ -4,6 +4,35 @@ require_once("model-size.php");
 $pageTitle = "Sizes";
 include "view-header.php";
 
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+  case "Add":
+    if (insertSizes($_POST['sizeName']))
+    {
+        echo '<div class="alert alert-success" role="alert">Size added.</div>';
+    }else{
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+    break;
+      case "Delete":
+    if (deleteSizes($_POST['sid']))
+    {
+        echo '<div class="alert alert-success" role="alert">Size deleted.</div>';
+    }else{
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+    break;
+    case "Edit":
+    if (updateSizes($_POST['sizeName'], $_POST['sid']))
+    {
+        echo '<div class="alert alert-success" role="alert">Size edited.</div>';
+    }else{
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+    break;
+  }
+}
+
 $sizes = selectSize();
 include "view-size.php";
 
