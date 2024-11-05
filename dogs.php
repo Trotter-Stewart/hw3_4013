0eb64c67cc2cbad5a4208def317865bb8632c52b
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-05 17:06:46 -0600

    Update dogs.php

--- dogs.php
+++ dogs.php
@@ -4,6 +4,35 @@ require_once("model-dogs.php");
 $pageTitle = "Dogs";
 include "view-header.php";
 
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+  case "Add":
+    if (insertDogs($_POST['dogName'], $_POST['dogCol'], $_POST['dogBreed'], $_POST['dogSize']))
+    {
+        echo '<div class="alert alert-success" role="alert">Breed added.</div>';
+    }else{
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+    break;
+      case "Delete":
+    if (deleteDogs($_POST['did']))
+    {
+        echo '<div class="alert alert-success" role="alert">Breed deleted.</div>';
+    }else{
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+    break;
+    case "Edit":
+    if (updateDogs($_POST['dogName'], $_POST['dogCol'] $_POST['dogBreed'] $_POST['dogSize'] $_POST['did']))
+    {
+        echo '<div class="alert alert-success" role="alert">Breed edited.</div>';
+    }else{
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+    }
+    break;
+  }
+}
+
 $dogs = selectDogs();
 include "view-dogs.php";
 
