5b98b46e32e4972c23003dc5b7ac7d00fd597427
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-13 16:52:57 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,15 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="//vjs.zencdn.net/8.19.1/video-js.min.css" rel="stylesheet">
-    <script src="//vjs.zencdn.net/8.19.1/video.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.css" />
     <style>
-      #my-player {
-          display: block;
-          margin: 0 auto;
-          width: 800px;
-    }
       body {
       background-image: url('https://www.shutterstock.com/image-vector/dog-seamless-pattern-paws-bones-600nw-2350264459.jpg');
       background-size: cover;
