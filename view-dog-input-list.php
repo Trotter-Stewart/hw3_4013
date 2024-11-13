6a8d7d077a0fb59c98b0d473479deafd27de750a
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-13 14:04:38 -0600

    Update view-dog-input-list.php

--- view-dog-input-list.php
+++ view-dog-input-list.php
@@ -1,8 +1,13 @@
 <select class="form-select" id="did" name="did">
 <?php
 while ($dogItem = $dogList->fetch_assoc()){
+   $selText = "";
+   if ($selectedDog == $dogItem['iddog'])
+   {
+      $selText = " selected";
+   }
 ?>
-   <option value="<?php echo $dogItem['dogid']; ?>"><?php echo $dogItem['dogname']; ?></option>
+   <option value="<?php echo $dogItem['dogid']; ?>"<?=$selText?>><?php echo $dogItem['dogname']; ?></option>
 <?php  
 }
 ?>
