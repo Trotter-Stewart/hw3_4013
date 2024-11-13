4a84f7f0df73b88e462f4adbb00f20accc59bae9
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-13 14:58:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1>Dawg Haus LLC</h1>
+https://www.pexels.com/video/woman-running-with-her-dog-2796084/
 <?php
 include "view-footer.php";
 ?>
