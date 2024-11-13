3665677169017acddae5d72d85caba69f7afdb2e
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-13 16:26:21 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,6 +7,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="//vjs.zencdn.net/8.19.1/video-js.min.css" rel="stylesheet">
     <script src="//vjs.zencdn.net/8.19.1/video.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.css" />
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.js"></script>
+    <script>AOS.init();</script>
     <style>
       #my-player {
           display: block;
