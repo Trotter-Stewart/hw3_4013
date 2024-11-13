995d4bb5721de502bcd0333656317542bc6a4af8
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-13 14:57:48 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="//vjs.zencdn.net/8.19.1/video-js.min.css" rel="stylesheet">
+    <script src="//vjs.zencdn.net/8.19.1/video.min.js"></script>
   </head>
   <body>
     <div class="container">
