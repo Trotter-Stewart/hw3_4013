96385bca849a7184b1c093bf11dfd46777561165
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-13 15:14:00 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,6 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="//vjs.zencdn.net/8.19.1/video-js.min.css" rel="stylesheet">
     <script src="//vjs.zencdn.net/8.19.1/video.min.js"></script>
+    <style>
+      #my-player {
+          display: block;
+          margin: 0 auto;
+          width: 800px;
+    }
+    </style>
   </head>
   <body>
     <div class="container">
