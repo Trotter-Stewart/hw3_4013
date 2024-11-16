a9eb5a0ebf2f47d9a606b6e72be624bc35f9c634
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-16 16:14:04 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -13,6 +13,13 @@
         color: white;
         border: none; 
       }
+      h1, h2, h3, h4, h5, h6 
+      {
+        background-color: #42A5F5;
+        color: white;
+        padding: 10px; 
+        border-radius: 5px;
+      }
     </style>
   </head>
   <body>
