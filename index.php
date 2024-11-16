edc636598beb57a629d7ee6d559bb166dcdd85da
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-16 17:01:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include "view-header.php";
       <img src="https://i.pinimg.com/originals/00/a3/4e/00a34e111574291422d03c95642f69e0.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://static.wikia.nocookie.net/thebluescluesencyclopedia/images/0/09/Blue-2D.jpeg/revision/latest/scale-to-width/360?cb=20220818013650" class="d-block w-100" alt="...">
+      <img src="https://www.parent.com/cdn/shop/articles/2559473304_17549860_900x.jpg?v=1608169835" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
       <img src="https://lwlies.com/wp-content/uploads/2017/08/air-bud-1997-1108x0-c-default.jpg" class="d-block w-100" alt="...">
