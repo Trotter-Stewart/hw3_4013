f8164add0c9ab38e629ef155f602ea29f9388adb
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-16 16:58:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ include "view-header.php";
     <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="https://i.pinimg.com/originals/00/a3/4e/00a34e111574291422d03c95642f69e0.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="https://static.wikia.nocookie.net/thebluescluesencyclopedia/images/0/09/Blue-2D.jpeg/revision/latest/scale-to-width/360?cb=20220818013650" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="https://lwlies.com/wp-content/uploads/2017/08/air-bud-1997-1108x0-c-default.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
