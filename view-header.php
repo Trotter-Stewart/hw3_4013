bd0eec57ea0e3d25bf65b08154d65b7925c04777
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-16 16:50:26 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -46,7 +46,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="nav justify-content-end">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
