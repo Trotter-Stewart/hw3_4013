a7fa21866b9c939c07e9307a6a3b11c65dec5e05
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-16 17:05:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ include "view-header.php";
     <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://i.pinimg.com/originals/00/a3/4e/00a34e111574291422d03c95642f69e0.jpg" class="d-block w-70" alt="...">
+      <img src="https://i.pinimg.com/originals/00/a3/4e/00a34e111574291422d03c95642f69e0.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://www.parent.com/cdn/shop/articles/2559473304_17549860_900x.jpg?v=1608169835" class="d-block w-70" alt="...">
+      <img src="https://www.parent.com/cdn/shop/articles/2559473304_17549860_900x.jpg?v=1608169835" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://lwlies.com/wp-content/uploads/2017/08/air-bud-1997-1108x0-c-default.jpg" class="d-block w-70" alt="...">
+      <img src="https://lwlies.com/wp-content/uploads/2017/08/air-bud-1997-1108x0-c-default.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
