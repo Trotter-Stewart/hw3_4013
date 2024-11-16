b46f1a4de2f46eb5e9c9fd0fe160672f946e619f
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-16 16:00:48 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,6 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.css" />
     <style>
+      .btn-warm-orange
+      {
+        background-color: #FFA726;
+        color: white;
+        border: none; 
+      }
     </style>
   </head>
   <body>
