93c557a5d934596e36b74ab677d9ff96ff200de8
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-30 14:20:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,26 +4,29 @@ include "view-header.php";
 ?>
     <h1>Dawg Haus</h1>
     </br>
-    <div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://i.pinimg.com/originals/00/a3/4e/00a34e111574291422d03c95642f69e0.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://www.parent.com/cdn/shop/articles/2559473304_17549860_900x.jpg?v=1608169835" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://lwlies.com/wp-content/uploads/2017/08/air-bud-1997-1108x0-c-default.jpg" class="d-block w-100" alt="...">
-    </div>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 </div>
 <?php
 include "view-footer.php";
