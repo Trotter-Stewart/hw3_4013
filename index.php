eb34d28369347cbdcaf36e8e4d6b461b74b39f02
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-30 14:37:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include "view-header.php";
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="https://images.articlesfactory.com/750x0/6aa89ecc-acf5-4008-b489-0d22762e226c.webp" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
