a9744bd6873247fc4af063efc94b1f9c63247be5
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-30 14:40:26 -0600

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ include "view-header.php";
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="https://i.pinimg.com/originals/00/a3/4e/00a34e111574291422d03c95642f69e0.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -38,7 +38,7 @@ include "view-header.php";
         </div>
     </div>
     <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="https://www.pedigreefoundation.org/media/uploads/2024/03/Secondary-Image-v2.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
