d0538eba33aaea36df5432b1071dde040c30460c
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-30 14:34:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include "view-header.php";
     </br>
 <div style="display: flex; gap: 16px; flex-wrap: wrap;">
     <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="https://www.fifty-five-plus.com/wp-content/uploads/2024/04/various-dog-breeds.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
