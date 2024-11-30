7e7865cc363abe3e0513bfbbc95f84e32995766c
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-30 14:16:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,24 +8,12 @@ include "view-header.php";
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://i.pinimg.com/originals/00/a3/4e/00a34e111574291422d03c95642f69e0.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
     </div>
     <div class="carousel-item">
       <img src="https://www.parent.com/cdn/shop/articles/2559473304_17549860_900x.jpg?v=1608169835" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
     </div>
     <div class="carousel-item">
       <img src="https://lwlies.com/wp-content/uploads/2017/08/air-bud-1997-1108x0-c-default.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
