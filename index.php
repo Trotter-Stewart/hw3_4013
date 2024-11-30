c34365dab4352a46252d0ef269f53e637fb0fdfa
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-30 14:26:36 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include "view-header.php";
 ?>
     <h1>Dawg Haus</h1>
     </br>
+<div style = "display: flex;">
 <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
@@ -26,6 +27,23 @@ include "view-header.php";
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
+</div>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 </div>
 </div>
 <?php
